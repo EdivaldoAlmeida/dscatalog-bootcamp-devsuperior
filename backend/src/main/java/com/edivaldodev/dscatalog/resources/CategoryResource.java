4dd00987f88767fed0bf3da6abd9e4da95815c0f
edivaldo.va@gmail.com <edivaldo.va@gmail.com>
2020-10-02 20:13:28 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/edivaldodev/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/edivaldodev/dscatalog/resources/CategoryResource.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edivaldodev.dscatalog.dto.CategoryDTO;
-import com.edivaldodev.dscatalog.entities.Category;
 import com.edivaldodev.dscatalog.services.CategoryService;
 
 @RestController
@@ -22,8 +22,15 @@ public class CategoryResource {
 	@GetMapping
 	public ResponseEntity<List<CategoryDTO>> findAll(){
 		List<CategoryDTO> list = service.findAll();
-		
 		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	// "/{id}" será acrescentado à rota básica "/categories"
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);	
+		return ResponseEntity.ok().body(dto);
 		
 	}
 
--- backend/src/main/java/com/edivaldodev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/edivaldodev/dscatalog/services/CategoryService.java
@@ -1,7 +1,7 @@
 package com.edivaldodev.dscatalog.services;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.edivaldodev.dscatalog.dto.CategoryDTO;
 import com.edivaldodev.dscatalog.entities.Category;
 import com.edivaldodev.dscatalog.repositories.CategoryRepository;
+import com.edivaldodev.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,9 +23,16 @@ public class CategoryService {
 	public List<CategoryDTO> findAll(){
 		
 		List<Category> list = repository.findAll();
-		
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		
-		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());		
 	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found!"));
+		
+		return new CategoryDTO(entity);
+	}
+	
+	
 }
