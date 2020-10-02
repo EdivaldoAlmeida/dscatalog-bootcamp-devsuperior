78db2bf2dab244391a05bd4622ba7fe5ff115545
edivaldo.va@gmail.com <edivaldo.va@gmail.com>
2020-10-02 07:52:45 -0300

    DTO

--- backend/src/main/java/com/edivaldodev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/edivaldodev/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.edivaldodev.dscatalog.services;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.edivaldodev.dscatalog.dto.CategoryDTO;
 import com.edivaldodev.dscatalog.entities.Category;
 import com.edivaldodev.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +19,12 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		
 	}
 }
