90eb42ced280babd7293b88632dc3053e0dd439e
edivaldo.va@gmail.com <edivaldo.va@gmail.com>
2020-10-10 08:20:02 -0300

    Finish home page

---
+++ front-web/src/core/components/Buttonicon/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ReactComponent as ArrowIncon } from '../../assets/images/arrow.svg';
+import './styles.scss';
+
+type Props = {
+    text: string;
+}
+
+const ButtonIcon = ( { text }: Props ) => (
+
+    <div className="d-flex">
+        <button className="btn btn-primary btn-icon">
+        <h5>
+            {text}
+        </h5>
+    </button>
+    <div className="btn-icon-content">
+        <ArrowIncon />
+    </div>
+    </div>
+        
+);
+
+export default ButtonIcon;
